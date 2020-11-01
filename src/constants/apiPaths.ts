2359c4123453be7778e92371a39ae5bd80e75256
uladzislau shastakou <uladzislau_shastakou@epam.com>
2020-11-01 22:54:24 +0300

    feat: add solution for task-3

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,8 @@ const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev'
+  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  products: 'https://dxrqxeehg8.execute-api.eu-west-1.amazonaws.com/dev/products'
 };
 
 export default API_PATHS;
