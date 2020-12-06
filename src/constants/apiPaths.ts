0bfaba19433c63e031da53ff806058ec78d87280
uladzislau shastakou <uladzislau_shastakou@epam.com>
2020-12-07 00:24:24 +0300

    feat: update cart link

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -5,7 +5,7 @@ const API_PATHS = {
   import: 'https://vfn4jaqrkj.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   products: 'https://dxrqxeehg8.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'http://shestak2039-cart-api-dev.eu-west-1.elasticbeanstalk.com/api',
+  cart: 'https://g45uoojkg3.execute-api.eu-west-1.amazonaws.com/shestak2039-cart-api/api',
 };
 
 export default API_PATHS;
