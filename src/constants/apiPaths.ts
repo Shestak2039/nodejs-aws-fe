9506edb25820ee3fbe847510d42a8b5d018d43ab
uladzislau shastakou <uladzislau_shastakou@epam.com>
2020-12-06 21:15:50 +0300

    feat: update front for task 8

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,8 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://vfn4jaqrkj.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  products: 'https://dxrqxeehg8.execute-api.eu-west-1.amazonaws.com/dev'
+  products: 'https://dxrqxeehg8.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://shestak2039-cart-api-dev.eu-west-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
